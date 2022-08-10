9f1097133f5cc86fb89516931cc36bfe3c3b2d2f
malrang <ruehd7924@gmail.com>
2022-08-10 22:50:02 +0900

    ✨[feat]: MainView navigationController item 추가 #5

--- ios-malrang-market/ios-malrang-market/Presentation/Main/View/MainViewController.swift
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/View/MainViewController.swift
@@ -7,14 +7,60 @@
 
 import UIKit
 
+private enum Const {
+    static let searchBarPlaceholder = "말랑마켓 통합검색"
+}
+
+private enum Image {
+    static let bookmark = "bookmark"
+    static let cart = "cart"
+}
+
 final class MainViewController: UIViewController {
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = Const.searchBarPlaceholder
+        return searchBar
+    }()
+
+    private let bookmarkBarButton: UIBarButtonItem = {
+        let configuration = UIImage.SymbolConfiguration(weight: .heavy)
+        let bookMarkImage = UIImage(systemName: Image.bookmark, withConfiguration: configuration)
+        let barButtonItem = UIBarButtonItem(
+            image: bookMarkImage,
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        barButtonItem.tintColor = #colorLiteral(red: 1, green: 0.8737915158, blue: 0.9193537831, alpha: 1)
+        return barButtonItem
+    }()
+
+    private let cartBarButton: UIBarButtonItem = {
+        let configuration = UIImage.SymbolConfiguration(weight: .heavy)
+        let cartImage = UIImage(systemName: Image.cart, withConfiguration: configuration)
+        let barButtonItem = UIBarButtonItem(
+            image: cartImage,
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        barButtonItem.tintColor = #colorLiteral(red: 1, green: 0.8737915158, blue: 0.9193537831, alpha: 1)
+        return barButtonItem
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
+        self.setupNavigationItem()
     }
 
     private func setupView() {
         self.view.backgroundColor = .systemBackground
+    }
+
+    private func setupNavigationItem() {
+        self.navigationItem.titleView = self.searchBar
+        self.navigationItem.rightBarButtonItems = [self.cartBarButton, self.bookmarkBarButton]
     }
 }
