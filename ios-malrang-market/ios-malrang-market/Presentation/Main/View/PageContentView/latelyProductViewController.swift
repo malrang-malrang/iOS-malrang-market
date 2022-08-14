91f2dcb24ffe29af3a8b224e9ce622e0c5fb756d
malrang <ruehd7924@gmail.com>
2022-08-15 06:15:53 +0900

    ➕[add]: LatelyProductViewController 파일추가 #13

---
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/View/PageContentView/latelyProductViewController.swift
@@ -0,0 +1,15 @@
+//
+//  latelyProductViewController.swift
+//  ios-malrang-market
+//
+//  Created by 김동욱 on 2022/08/15.
+//
+
+import UIKit
+
+final class LatelyProductViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
