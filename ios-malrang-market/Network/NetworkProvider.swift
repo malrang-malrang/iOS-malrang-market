008f21b278e98c96c1c56c2e8d86045498f1881c
malrang <ruehd7924@gmail.com>
2022-08-16 23:48:42 +0900

    ✨[feat]: NetworkProvider checkError()메서드 구현 #13

--- ios-malrang-market/Network/NetworkProvider.swift
+++ ios-malrang-market/Network/NetworkProvider.swift
@@ -31,4 +31,29 @@ final class NetworkProvider: Provider {
     init(urlSession: URLSessionProtocol = URLSession.shared) {
         self.urlSession = urlSession
     }
+
+    private func checkError(
+        with data: Data?,
+        _ response: URLResponse?,
+        error: Error?,
+        completionHandler: @escaping (Result<Data, Error>) -> Void
+    ) {
+        if let error = error {
+            return completionHandler(.failure(error))
+        }
+
+        guard let response = response as? HTTPURLResponse else {
+            return completionHandler(.failure(NetworkError.responseError))
+        }
+
+        guard (200..<300).contains(response.statusCode) else {
+            return completionHandler(.failure(NetworkError.statusCodeError(statusCode: response.statusCode)))
+        }
+
+        guard let data = data else {
+            return completionHandler(.failure(NetworkError.emptyDataError))
+        }
+
+        completionHandler(.success(data))
+    }
 }
