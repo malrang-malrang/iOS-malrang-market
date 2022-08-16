9a85bac981af4a10fec8afb63ae1102080531d7f
malrang <ruehd7924@gmail.com>
2022-08-16 23:36:49 +0900

    ✨[feat]: HTTPMethod 추가및 구현 #13

---
+++ ios-malrang-market/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  ios-malrang-market
+//
+//  Created by 김동욱 on 2022/08/16.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
