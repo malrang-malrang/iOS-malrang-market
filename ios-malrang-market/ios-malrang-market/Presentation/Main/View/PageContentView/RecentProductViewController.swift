7cdcddf7db6ca4ce320f3f6ff0d1099fad43be63
malrang <ruehd7924@gmail.com>
2022-08-22 01:05:26 +0900

    ♻️[refactor]: LatelyProductViewController 네이밍 변경 및 구현 #15
    * LatelyProductViewController -> RecentProductViewController

---
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/View/PageContentView/RecentProductViewController.swift
@@ -0,0 +1,36 @@
+//
+//  latelyProductViewController.swift
+//  ios-malrang-market
+//
+//  Created by 김동욱 on 2022/08/15.
+//
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class RecentProductViewController: UICollectionViewController {
+    private let tableView = UITableView()
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+        self.setupConstraint()
+        self.bind()
+    }
+
+    private func setupView() {
+        self.view.addSubviews(self.tableView)
+    }
+
+    private func setupConstraint() {
+        self.tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    private func bind() {
+        
+    }
+}
