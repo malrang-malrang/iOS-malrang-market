660b4a8c2fe6e21aa155914a3125446ff7d38c3a
malrang <ruehd7924@gmail.com>
2022-08-22 19:28:56 +0900

    ♻️[refactor]: MainViewModel useCase 주입 받도록 변경 #15

--- ios-malrang-market/ios-malrang-market/Presentation/Main/Presenter/MainViewModel.swift
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/Presenter/MainViewModel.swift
@@ -22,7 +22,13 @@ protocol MainViewModelOutput {
 protocol MainViewModelable: MainViewModelInput, MainViewModelOutput {}
 
 final class MainViewModel: MainViewModelable {
+    private let useCase: Usecase
     let pageState = BehaviorRelay<Page>(value: .recentProduct)
+    let recentProducts = BehaviorRelay<ProductList>(value: ProductList())
+
+    init(useCase: Usecase) {
+        self.useCase = useCase
+    }
 
     func didTapSegmentControl(selected index: Int) {
         guard let index = Page(rawValue: index) else {
@@ -30,5 +36,17 @@ final class MainViewModel: MainViewModelable {
         }
 
         self.pageState.accept(index)
+    }
+
+    func fetchRecentProductList(pageNumber: Int, perPages: Int) {
+        _ = self.useCase.fetchProductList(pageNumber: pageNumber, perPages: perPages)
+            .subscribe { productList in
+                guard let recentProducts = productList else {
+                    return
+                }
+                self.recentProducts.accept(recentProducts)
+            } onFailure: { error in
+                return print(error)
+            }
     }
 }
