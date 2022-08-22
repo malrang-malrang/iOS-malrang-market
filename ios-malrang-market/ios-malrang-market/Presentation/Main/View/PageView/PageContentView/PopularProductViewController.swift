da0a1e544f59bbe449ea3c944d46a82fec523cce
malrang <ruehd7924@gmail.com>
2022-08-23 00:00:57 +0900

    ♻️[refactor]: PopularProductView init 수정 #15

--- ios-malrang-market/ios-malrang-market/Presentation/Main/View/PageView/PageContentView/PopularProductViewController.swift
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/View/PageView/PageContentView/PopularProductViewController.swift
@@ -5,18 +5,14 @@
 //  Created by 김동욱 on 2022/08/22.
 //
 
-import RxCocoa
-import RxSwift
-import SnapKit
+import UIKit
 
 final class PopularProductViewController: UIViewController {
     private let viewModel: MainViewModelable
-    private let collectionView = UICollectionView()
-    private let disposeBag = DisposeBag()
 
     init(viewModel: MainViewModelable) {
         self.viewModel = viewModel
-        super.init()
+        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder: NSCoder) {
@@ -25,22 +21,5 @@ final class PopularProductViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupView()
-        self.setupConstraint()
-        self.bind()
-    }
-
-    private func setupView() {
-        self.view.addSubviews(self.collectionView)
-    }
-
-    private func setupConstraint() {
-        self.collectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-    }
-
-    private func bind() {
-
     }
 }
