64b86e385a2b2364b11709a7a70e804c06af0990
malrang <ruehd7924@gmail.com>
2022-08-22 23:58:47 +0900

    ➕[add]: RecentProductListCell 추가 #15

---
+++ ios-malrang-market/ios-malrang-market/Presentation/Main/View/PageView/PageContentView/Cell/RecentProductListCell.swift
@@ -0,0 +1,14 @@
+//
+//  RecentProductListCell.swift
+//  ios-malrang-market
+//
+//  Created by 김동욱 on 2022/08/22.
+//
+
+import UIKit
+
+final class RecentProductListCell: UITableViewCell {
+    static var identifier: String {
+        String(describing: Self.self)
+    }
+}
