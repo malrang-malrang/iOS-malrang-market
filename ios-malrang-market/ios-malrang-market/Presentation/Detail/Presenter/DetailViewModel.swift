95a923b49b4720923632cd5e30c5ccf07af80fbd
malrang <ruehd7924@gmail.com>
2022-09-03 00:43:18 +0900

    ⚰️[del]: DetailViewModel imageCount 프로퍼티 제거 #17

--- ios-malrang-market/ios-malrang-market/Presentation/Detail/Presenter/DetailViewModel.swift
+++ ios-malrang-market/ios-malrang-market/Presentation/Detail/Presenter/DetailViewModel.swift
@@ -15,7 +15,6 @@ protocol DetailViewModelInput {
 
 protocol DetailViewModelOutput {
     var productImages: BehaviorRelay<[UIImage]> { get }
-    var imageCount: BehaviorRelay<Int> { get }
     func productInfomation() -> ProductInfomation
 }
 
@@ -26,7 +25,6 @@ final class DetailViewModel: DetailViewModelable {
     private let useCase: Usecase
     private let coordinator: DetailViewCoordinatorProtocol
     let productImages = BehaviorRelay<[UIImage]>(value: [])
-    let imageCount = BehaviorRelay<Int>(value: .zero)
 
     init(
         product: ProductDetail,
@@ -64,7 +62,6 @@ extension DetailViewModel {
             .subscribe { [weak self] images in
                 let imageList = images.compactMap { $0.url?.image() }
                 self?.productImages.accept(imageList)
-                self?.imageCount.accept(images.count)
             } onError: { error in
                 print(error)
             }
