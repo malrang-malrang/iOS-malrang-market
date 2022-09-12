fbed5639cbcb3a6bc301e22e416c28ffb75ce006
malrang <ruehd7924@gmail.com>
2022-09-13 04:04:40 +0900

    ✅[chore]: ProductError -> InputError 네이밍 변경및 케이스 추가 #25

--- ios-malrang-market/ios-malrang-market/Presentation/Error/InputError.swift
+++ ios-malrang-market/ios-malrang-market/Presentation/Error/InputError.swift
@@ -7,16 +7,19 @@
 
 import Foundation
 
-enum ProductError: Error {
+enum InputError: Error {
     case hasNextPage
     case productId
+    case productImage
 
-    var errorDescription: String? {
+    var errorDescription: String {
         switch self {
         case .hasNextPage:
             return "다음 페이지 정보가 없습니다."
         case .productId:
             return "상품 아이디가 잘못 되었습니다."
+        case .productImage:
+            return "상품 이미지를 가져올수 없습니다."
         }
     }
 }
