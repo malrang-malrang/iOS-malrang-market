0d7e70627f2b2c946e704bd9fc1f0754e56b8e45
malrang <ruehd7924@gmail.com>
2022-09-16 17:54:29 +0900

    ✨[feat]: SystemImage enum 추가및 구현 #32

---
+++ ios-malrang-market/ios-malrang-market/Presentation/SystemImage/SystemImage.swift
@@ -0,0 +1,34 @@
+//
+//  SystemImage.swift
+//  ios-malrang-market
+//
+//  Created by 김동욱 on 2022/09/16.
+//
+
+import UIKit
+
+enum SystemImage {
+    enum Atribute {
+        static let configuration = UIImage.SymbolConfiguration(pointSize: 30, weight: .heavy)
+    }
+
+    static let back = UIImage(
+        systemName: "arrowshape.turn.up.backward.fill",
+        withConfiguration: Atribute.configuration
+    )
+
+    static let more = UIImage(
+        systemName: "ellipsis",
+        withConfiguration: Atribute.configuration
+    )
+
+    static let check = UIImage(
+        systemName: "checkmark.diamond.fill",
+        withConfiguration: Atribute.configuration
+    )
+
+    static let edit = UIImage(
+        systemName: "square.and.pencil",
+        withConfiguration: Atribute.configuration
+    )
+}
